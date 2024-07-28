08ad829e7496cb21490dff68f3cac94ca3907080
kubapoke <q.paczka@gmail.com>
2024-07-29 00:23:28 +0200

    Moved to .NET 8.0; Started implementing A* Solver

---
+++ MazeGenerator/Maze/Solvers/AStarSolver.cs
@@ -0,0 +1,24 @@
+﻿using MazeGenerator.Drawing;
+using MazeGenerator.Drawing.SolutionDrawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeGenerator.Maze.Solvers
+{
+    public class AStarSolver : Solver
+    {
+        public override (List<int> solution, Drawer drawer) SolveMaze(List<int>[] mazeGraph, int width, int height, (int x, int y) start, (int x, int y) finish)
+        {
+            List<int> solution = new List<int>();
+            // PriorityQueue<int, int>
+            bool[] visited = new bool[width * height];
+            int[] prev = new int[width * height];
+            Drawer drawer = new ParallelPathSolutionDrawer(MainWindow.Canvas, width, height);
+
+            throw new NotImplementedException();
+        }
+    }
+}
